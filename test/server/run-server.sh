d5c9eb8870d3f8d11856808d9f575badefdeec30
Josh Baker <joshbaker77@gmail.com>
2015-07-05 08:16:13 -0700

    micro server in go

---
+++ test/server/run-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export GOPATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+cd $GOPATH
+
+go get "github.com/gorilla/websocket"
+go run server.go
---
+++ test/server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":6789", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("connection established")
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer func() {
+		ws.Close()
+		log.Print("connection closed")
+	}()
+
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		log.Print("rcvd: '" + string(msg) + "'")
+	}
+}
